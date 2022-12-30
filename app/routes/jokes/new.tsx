6ff6211ea156302aee07a1adbeefa7184a56432d
mario-aj <mariojorge1997@mail.ru>
2022-12-30 17:44:05 +0100

    learning how to work with remix action fuction

--- app/routes/jokes/new.tsx
+++ app/routes/jokes/new.tsx
@@ -1,21 +1,40 @@
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+
+import { db } from "~/utils/db.server";
+
+export async function action({ request }: ActionArgs) {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+
+  if (typeof name !== "string" || typeof content !== "string")
+    throw new Error("Form not submitted correctly.");
+
+  const fileds = { name, content };
+
+  const joke = await db.joke.create({
+    data: fileds,
+  });
+
+  return redirect(`/jokes/${joke.id}`);
+}
+
 export default function NewJokeRoute() {
   return (
     <div>
       <p>Add your own hilarious joke</p>
-
       <form method="post">
         <div>
           <label>
             Name: <input type="text" name="name" />
           </label>
         </div>
-
         <div>
           <label>
             Content: <textarea name="content" />
           </label>
         </div>
-
         <div>
           <button type="submit" className="button">
             Add
