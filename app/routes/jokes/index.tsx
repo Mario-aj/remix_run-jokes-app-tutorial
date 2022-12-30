fc06bea3edec45e816864736ced7223a3d2b6583
mario-aj <mariojorge1997@mail.ru>
2022-12-30 17:26:57 +0100

    getting a random joke from db

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,27 +1,36 @@
 import type { LinksFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
 
 import stylesUrl from "~/styles/index.css";
+import { db } from "~/utils/db.server";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: stylesUrl }];
 };
 
+export const loader = async () => {
+  const count = await db.joke.count();
+  const randomRowNumber = Math.floor(Math.random() * count);
+
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+
+  return json({
+    randomJoke,
+  });
+};
+
 export default function IndexRoute() {
+  const { randomJoke } = useLoaderData<typeof loader>();
+
   return (
-    <div className="container">
-      <div className="content">
-        <h1>
-          Remix <span>Jokes!</span>
-        </h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="jokes">Read Jokes</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{randomJoke.content}</p>
+      <Link to={randomJoke.id}>"{randomJoke.name}" Permalink</Link>
     </div>
   );
 }
