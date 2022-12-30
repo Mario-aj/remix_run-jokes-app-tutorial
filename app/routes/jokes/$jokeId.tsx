084bcf943f1817fc294c4aad050b38378e6e3113
mario-aj <mariojorge1997@mail.ru>
2022-12-30 17:17:48 +0100

    rendering joke by id

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,29 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+
+  return json({ joke });
+};
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData<typeof loader>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{joke.content}</p>
+      <Link to=".">{joke.name}</Link>
     </div>
   );
 }
