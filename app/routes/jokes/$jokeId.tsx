4436268ba62c6273fae69c83178db5941384629c
mario-aj <mariojorge1997@mail.ru>
2023-01-01 14:37:13 +0100

    deliting joke feature

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,10 +1,12 @@
-import type { LoaderArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
+import type { LoaderArgs, ActionArgs } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { Link, useCatch, useLoaderData, useParams } from "@remix-run/react";
 
 import { db } from "~/utils/db.server";
+import { getUserId } from "~/utils/sesseion.server";
 
-export const loader = async ({ params }: LoaderArgs) => {
+export const loader = async ({ params, request }: LoaderArgs) => {
+  const userId = await getUserId(request);
   const joke = await db.joke.findUnique({
     where: { id: params.jokeId },
   });
@@ -15,22 +17,65 @@ export const loader = async ({ params }: LoaderArgs) => {
     });
   }
 
-  return json({ joke });
+  return json({ joke, isOwner: userId === joke.jokesterId });
 };
+
+export async function action({ request, params }: ActionArgs) {
+  const form = await request.formData();
+
+  if (form.get("intent") !== "delete")
+    throw new Response(`The intent ${form.get("intent")} is not supported`, {
+      status: 400,
+    });
+
+  const userId = await getUserId(request);
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+
+  if (!joke)
+    throw new Response("Can't delete what does not exist.", {
+      status: 404,
+    });
+
+  if (joke.jokesterId !== userId)
+    throw new Response("Pssh, That's not your joke", {
+      status: 403,
+    });
+
+  await db.joke.delete({ where: { id: params.jokeId } });
+  return redirect("/jokes");
+}
 
 export function CatchBoundary() {
   const caught = useCatch();
   const params = useParams();
-
-  if (caught.status === 404) {
-    return (
-      <div className="error-container">
-        Huh? What the heck is "{params.jokeId}"?
-      </div>
-    );
+  switch (caught.status) {
+    case 400: {
+      return (
+        <div className="error-container">
+          What you're trying to do is not allowed.
+        </div>
+      );
+    }
+    case 404: {
+      return (
+        <div className="error-container">
+          Huh? What the heck is {params.jokeId}?
+        </div>
+      );
+    }
+    case 403: {
+      return (
+        <div className="error-container">
+          Sorry, but {params.jokeId} is not your joke.
+        </div>
+      );
+    }
+    default: {
+      throw new Error(`Unhandled error: ${caught.status}`);
+    }
   }
-
-  throw new Error(`Unhandled error: ${caught.status}`);
 }
 
 export function ErrorBoundary() {
@@ -42,13 +87,20 @@ export function ErrorBoundary() {
 }
 
 export default function JokeRoute() {
-  const { joke } = useLoaderData<typeof loader>();
+  const { joke, isOwner } = useLoaderData<typeof loader>();
 
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <p>{joke.content}</p>
       <Link to=".">{joke.name}</Link>
+      {isOwner ? (
+        <form method="post">
+          <button className="button" name="intent" type="submit" value="delete">
+            Delete
+          </button>
+        </form>
+      ) : null}
     </div>
   );
 }
